cd4abc760f0ba04d69fd43c66ab9b7d053146565
asadfsko <serv27680@gmail.com>
2025-04-13 19:50:33 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const items = {
+  Streetwear: { owned: 0, baseValue: 10, demand: 0.5 },
+  Sneakers: { owned: 0, baseValue: 50, demand: 0.4, unlocked: false },
+  // Add the rest of your JavaScript logic here
+};
+
+function updateUI() {
+  // Update UI logic here
+}
+
+// Add the rest of your JavaScript functions here
